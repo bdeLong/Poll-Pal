06fb2f68a986c790fbe533db80d69f44510feab4
Ben deLong <bendelong45@gmail.com>
2018-10-16 21:36:38 -0500

    added firebase database config for later use

---
+++ app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA_65abpPMuzwxk_LkmbvamOunnZhUjqSk",
+  authDomain: "poll-pal-219318.firebaseapp.com",
+  databaseURL: "https://poll-pal-219318.firebaseio.com",
+  projectId: "poll-pal-219318",
+  storageBucket: "poll-pal-219318.appspot.com",
+  messagingSenderId: "846517490745"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
